2b5f13d9c5b11b994bcdbdb6a99f2723285eb82b
Tiago Royer <royertiago@gmail.com>
2014-09-18 13:18:27 -0300

    Lexical analysis now is executed only if command line option -l is specified

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
+#include <cstring>
 #include "lexer.h"
 
-int main( int argc, char * argv[] ) {
-    if( argc != 2 ) {
-        std::cerr << "Usage: " << argv[0] << " <filename>\n";
-        return 1;
-    }
-    Lexer alex( argv[1] );
+void lexical_analysis( const char * filename ) {
+    Lexer alex( filename );
 
     while( alex.has_next() )
     {
@@ -14,7 +11,33 @@ int main( int argc, char * argv[] ) {
         std::cout << "Id: " << tok.c_str() << ", Lexeme: '" <<
             tok.lexeme << "' - " << tok.line << ':' << tok.column << '\n';
     }
+}
 
+
+int main( int argc, char * argv[] ) {
+    char * filename;
+    bool lexical = false;
+
+    if( argc == 2 )
+        filename = argv[1];
+    else if( argc == 3 ) {
+        filename = argv[2];
+        if( strcmp(argv[1], "-l" ) == 0 )
+            lexical = true;
+        else {
+            std::cerr << "Unknown option " << argv[1] << '\n';
+            return 1;
+        }
+    }
+    else {
+        std::cerr << "Usage: " << argv[0] << " [-l] <filename>\n";
+        return 1;
+    }
+
+    if( lexical )
+        lexical_analysis( filename );
+    else
+        std::cerr << "Syntatical analysis unimplemented\n";
     return 0;
 }
 
