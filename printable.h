b68ceced2cff129d102b099da32b20bc80851473
Tiago Royer <royertiago@gmail.com>
2014-12-11 16:23:45 -0200

    printable.h: Documented design decision in Printable::clone.

--- printable.h
+++ printable.h
@@ -10,7 +10,15 @@
 struct Printable {
     virtual ~Printable() = default;
     virtual std::ostream& print_to( std::ostream& ) const = 0;
-    virtual Printable * clone() const = 0;
+
+    /* Returs a deep copy of this object.
+     * The ownterwship of the returned pointer is transferred
+     * to the caller; it is its responsibility to delete afterwards
+     * (or to assign to a smart pointer).
+     *
+     * This method returs a raw pointer instead of a smart pointer
+     * to allow covariance. */
+     virtual Printable * clone() const = 0;
 };
 
 inline std::ostream& operator<<( std::ostream& os, const Printable& obj ) {
