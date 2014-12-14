5ebe7cd7dc8b867564f250f5348584409ccb68b7
Tiago Royer <royertiago@gmail.com>
2014-12-14 18:51:11 -0200

    main.cpp: Add operator evaluation to user interface
    The semantics of the language says that the last operator should
    be nullary and the program evaluation consists in computing this
    operator. Computing the last operator is now the default behavior
    of the interpreter.
    
    With this commit, we implement all the basic features
    of the language in an interpreter.
    It remains to implement an interactive mode and fix all bugs.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "lexer.h"
 #include "parser.h"
 #include "semantic_analyser.h"
+#include "symbol_table.h"
 
 void lexical_analysis( const char * filename ) {
     Lexer alex( filename );
@@ -34,31 +35,56 @@ void semantic_analysis( const char * filename ) {
         try {
             std::cout << *semantic_analyser.next() << '\n';
         } catch ( parse_error& ex ) {
-            std::cerr << "Syntagtic error: " << ex.what()
+            std::cerr << "Syntactic error: " << ex.what()
                       << ' ' << ex.where.line << ':' << ex.where.column << '\n';
         } catch ( semantic_error & ex ) {
             std::cerr << "Semantic error: " << ex.what() << '\n';
         }
 }
 
+void run_program( const char * filename ) {
+    SemanticAnalyser analyser;
+    analyser.set_parser( std::make_unique<Parser>(filename) );
+    bool errors = false;
+    while( analyser.has_next() )
+        try {
+            analyser.next();
+        } catch ( parse_error& ex ) {
+            std::cerr << "Syntactic error: " << ex.what()
+                      << ' ' << ex.where.line << ':' << ex.where.column << '\n';
+            errors = true;
+        } catch ( semantic_error & ex ) {
+            std::cerr << "Semantic error: " << ex.what() << '\n';
+            errors = true;
+        }
+
+    if( errors ) {
+        std::cerr << "Aborting due to programming errors.";
+        return;
+    }
+
+    std::cout << *SymbolTable::lastNullaryInserted()->compute() << std::endl;
+}
+
 int main( int argc, char * argv[] ) {
     if( argc == 2 ) {
         if( strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0 ) {
-            std::cout << "Usage: " << argv[0] << " [-l | -p | -s] <filename>\n"
+            std::cout << "Usage: " << argv[0] << " [-l | -p | -s | -r] <filename>\n"
                          "\n"
                          "This program will analyse the program specified in the last argument.\n"
                          "  -l, --lexer     Do lexical analysis on the program.\n"
                          "  -p, --parser    Do syntactic analysis on the program.\n"
-                         "  -s, --semantic  Do semantical analysis on the program. This is the default.\n"
+                         "  -s, --semantic  Do semantical analysis on the program.\n"
+                         "  -r, --run       Run the program. This is the default.\n"
                          "  -h, --help      Display this help and quit.\n";
             return 0;
         }
-        semantic_analysis( argv[1] );
+        run_program( argv[1] );
         return 0;
     }
 
     if( argc != 3 ) {
-        std::cout << "Usage: " << argv[0] << " [-l | -p | -s] <filename>\n";
+        std::cout << "Usage: " << argv[0] << " [-l | -p | -s | -r] <filename>\n";
         return 1;
     }
 
@@ -75,7 +101,10 @@ int main( int argc, char * argv[] ) {
         semantic_analysis( filename );
         return 0;
     }
-
+    if( strcmp(argv[1], "-r" ) == 0 || strcmp(argv[1], "--run") == 0 ) {
+        run_program( filename );
+        return 0;
+    }
 
     std::cerr << "Unknown option " << argv[1] << '\n';
     return 1;
