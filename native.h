783d4987c9f439ebae5fcc92e2202585b6eb5f82
Tiago Royer <royertiago@gmail.com>
2014-12-15 13:01:48 -0200

    native.h: generate NativeOperators from C++ operators
    The function insertNative (aided by the class NativeBinaryNumericOperator)
    automates the conversion from a C++ binary functor to a NativeOperator.

--- native.h
+++ native.h
@@ -4,12 +4,51 @@
 #ifndef NATIVE_H
 #define NATIVE_H
 
+#include <ostream>
 #include "ast.h"
+#include "symbol_table.h"
 
 struct NativeOperation : public OperatorBody {
-    virtual std::unique_ptr<Variable> evaluate( const VariableTable& ) = 0;
+    virtual std::unique_ptr<Variable> evaluate( const VariableTable& ) const = 0;
     virtual ~NativeOperation() = default;
-    virtual OperatorBody * clone() const override = 0;
+    virtual NativeOperation * clone() const override = 0;
 };
+
+/* Template for binary numeric operations.
+ * This class assumes either xfx, xfy or yfx format and variables
+ * {X} and {Y}. */
+template< typename Functor >
+struct NativeBinaryNumericOperator : public NativeOperation {
+    Functor f;
+    std::string name;
+
+    NativeBinaryNumericOperator( Functor f, std::string name ): f(f), name(name) {}
+
+    virtual std::unique_ptr<Variable> evaluate( const VariableTable& table ) const override {
+        auto X = table.retrieve("X");
+        auto Y = table.retrieve("Y");
+        return std::make_unique<Variable>( f(X->value, Y->value) );
+    }
+    virtual NativeBinaryNumericOperator * clone() const override {
+        return new NativeBinaryNumericOperator{ f, name };
+    }
+    virtual std::ostream& print_to( std::ostream& os ) const override {
+        return os << "{Native " << name << "}";
+    }
+};
+
+template< typename Functor >
+void insertNative( std::string name, std::string format, unsigned priority, Functor f ) {
+    Token X, Y;
+    X.lexeme = "X";
+    Y.lexeme = "Y";
+    auto ptr = std::make_unique<BinaryOverload>(
+            name,
+            std::make_unique<NativeBinaryNumericOperator<Functor>>(f, name),
+            std::make_unique<RestrictedParameter>(X),
+            std::make_unique<RestrictedParameter>(Y)
+        );
+    SymbolTable::insertOverload( name, format, priority, std::move(ptr) );
+}
 
 #endif // NATIVE_H
